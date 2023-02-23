f556b756d4e7ef443b76a6eccdc2b1a220100208
설동훈 <tjfehdgns@gmail.com>
2023-02-23 18:55:27 +0900

    need to add scroll one time only

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import './style.dart' as style;
+import 'package:http/http.dart' as http;
+import 'dart:convert';  //유틸리티
 
 void main() {
   runApp( MaterialApp(
@@ -18,6 +20,26 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
 
+  var data = [];
+
+  getData() async {
+    var site = await http.get(Uri.parse('https://codingapple1.github.io/app/data.json'));
+    if(site.statusCode == 200){print('서버 좋음');}
+    else{print('서버 이상함');}
+
+    var result = jsonDecode(site.body);
+    setState(() {
+      data = result;
+    });
+    print(site.body);
+    print(result[0]['likes']);
+  }  //initState 안에 async 사용불가
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  } //  특정 위젯이 로드되자마자 코드를 실행
+
   var tab = 0;
   void _onItemTapped(int index) {
     setState(() {
@@ -25,7 +47,11 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
-
+  addData(a){
+    setState(() {
+      data.add(a);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +61,7 @@ class _MyAppState extends State<MyApp> {
             onPressed: (){},
             icon: Icon(Icons.add_box_outlined))],
         title: Text('Instagram', style: TextStyle(color: Colors.black, fontSize: 20,fontWeight: FontWeight.bold),)),
-      body: [page(), Text('숍')][tab],
+      body: [page(data: data, addData : addData), Text('숍')][tab],
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: true,
         showUnselectedLabels: false,
@@ -58,44 +84,67 @@ class _MyAppState extends State<MyApp> {
 
 //홈페이지
 class page extends StatefulWidget {
-  const page({Key? key,}) : super(key: key);
-
+  const page({Key? key, this.data, this.addData}) : super(key: key);
+  final data, addData;
 
   @override
   State<page> createState() => _pageState();
 }
 
 class _pageState extends State<page> {
-  var likes = 0;
-  addOne() {
-    setState(() {
-      likes++;
+
+  var scroll = ScrollController();
+
+  getMore() async {
+    var result = await http.get(Uri.parse('https://codingapple1.github.io/app/more1.json'));
+    var result2 = jsonDecode(result.body);
+    widget.addData(result2);
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    scroll.addListener(() {
+      print(scroll.position.pixels);
+      if(scroll.position.pixels == scroll.position.maxScrollExtent){
+        print('마지막 스크롤');
+        setState(() {
+          getMore();
+        });
+      }
     });
   }
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        scrollDirection: Axis.vertical,
-        itemCount: 3,
-        itemBuilder: (context, index) {
-          return Column(children: [
-            Image.network('https://codingapple1.github.io/kona.jpg'),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
+    if (widget.data.isNotEmpty){
+      return ListView.builder(
+      scrollDirection: Axis.vertical,
+      controller: scroll,
+      padding: EdgeInsets.all(10),
+      itemCount: widget.data.length,
+      itemBuilder: (context, index) {
+        return Column(children: [
+          Image.network(widget.data[index]['image']),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
               Row(
                 children: [
-                  TextButton(onPressed: () {addOne();}, child: Text('좋아요 : ')),
-                  Text(likes.toString())
+                  IconButton(onPressed: () {}, icon: Icon(Icons.thumb_up_sharp)),  //좋아요 state 못바꿈
+                  Text('${widget.data[index]['likes'].toString()}  좋아요')
                 ],
               ),
-              Text('글쓴이 : ', textAlign: TextAlign.start,),
-              Text('글내용 :  ', textAlign: TextAlign.start),
-
+              Text(widget.data[index]['user'] , textAlign: TextAlign.start,),
+              Text(widget.data[index]['content'], textAlign: TextAlign.start),
             ],)
-          ]);
-          },
+        ]);
+      },
     );
+
+    } else {
+      return Text('로딩중');
+    }
   }
 }
 
